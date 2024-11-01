8bad505fe0f9bf19ac662951d0e30a6586f6533b
soficuozzo <2313251@ucc.edu.ar>
2024-11-01 00:51:55 -0300

    Conexion con mongo, ok

--- cursos-api/main.go
+++ cursos-api/main.go
@@ -1,1 +1,29 @@
-package cursosapi
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "proyecto_arqui_soft_2/cursos-api/repositories"
+)
+
+func main() {
+    config := repositories.MongoConfig{
+        Host:       "localhost",   
+        Port:       "27017",       
+        Username:   "sofia",  
+        Password:   "1234",  
+        Database:   "cursos_db",
+        Collection: "cursos",
+    }
+
+    mongoRepo := repositories.NewMongo(config)
+    ctx := context.Background()
+
+    if err := mongoRepo.TestConnection(ctx); err != nil {
+        log.Fatalf("MongoDB connection failed: %v", err)
+    } else {
+        fmt.Println("MongoDB connection test successful!")
+    }
+}
+
